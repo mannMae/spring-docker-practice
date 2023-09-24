705917e99ff33d53a0c18b19bf45958277aaf061
Jaeman Kim <daga4242@gmail.com>
2023-09-24 16:53:22 +0900

    230924 hello-spring

---
+++ 23_09_24/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,29 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+import java.util.List;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){this.memberRepository = memberRepository; }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원이름입니다.");
+        });
+    }
+
+    public List<Member> findMembers() { return memberRepository.findAll(); }
+}
